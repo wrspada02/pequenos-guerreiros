58b314816c249a56bcc5a04ac559d66efb55c64e
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2023-11-11 23:16:15 -0300

    feat: finish header and footer

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,15 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-primary': 'linear-gradient(180deg, rgba(0,137,189,1) 15%, rgba(1,149,51,0.55) 33%);'
       },
+      maxWidth: {
+        'logo': '10ch'
+      },
+      fontFamily: {
+        'logo': 'Suez One, serif',
+        'rubik': 'Rubik, sans-serif'
+      }
     },
   },
   plugins: [],
